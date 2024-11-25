bbbfc4cb26bab6247bdd9c70d88d431dd4445eb3
domhnail <coledanodonnell@hotmail.com>
2024-11-25 11:08:10 -0400

    React, started outlet routing.

--- client/cawfee-ui/src/App.jsx
+++ client/cawfee-ui/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { Outlet } from 'react-router-dom';
+import Sidebar from './ui/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <h1>welcome to cawfee</h1>
+    <p>This is the master page.</p>
+    <div>
+      <Sidebar />
+    </div>
+    <div>
+      <br></br>
+      <Outlet />
+      <p>This is the child page.</p>
+    </div>
     </>
   )
 }
